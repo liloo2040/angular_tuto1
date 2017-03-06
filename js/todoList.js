bde5133eb87991f1e7398a713096f608e44410a0
liloo2040 <e.dufour.89@gmail.com>
2017-03-06 22:41:49 +0100

    ajout module

---
+++ js/todoList.js
@@ -0,0 +1,10 @@
+//js/todoList.js
+'use strict';
+
+/****Déclaration de l'appli demoApp ***/
+
+var demoApp = angular.module('demoApp', [
+	//Dépendance du "module"
+	'todoList']);
+
+var todoList = angular.module('todoList', []);
