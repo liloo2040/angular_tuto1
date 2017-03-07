bb84cdacd352d1784b82bbc8fdd0520692bee158
liloo2040 <e.dufour.89@gmail.com>
2017-03-07 18:44:44 +0100

    ajout controller bootstrap et lien css

--- js/todoList.js
+++ js/todoList.js
@@ -8,3 +8,48 @@ var demoApp = angular.module('demoApp', [
 	'todoList']);
 
 var todoList = angular.module('todoList', []);
+
+/* Controller de l'appli */
+todoList.controller('todoCtrl', ['$scope',
+	function($scope){
+		//Pour manipuler les todos au sein du controller, on initialise les todos avec un tableau vide
+		var todos = $scope.todos = [];
+
+
+		//Ajouter un todo
+
+		$scope.addTodo = function() {
+			var newTodo = $scope.newTodo.trim();
+
+		//.trim réduit les espaces inutiles au début et à la fin d'une chaine de caractères
+
+			if(!newTodo.length){
+				return;
+			}
+			todos.push({
+				title: newTodo,
+				completed: false
+			});
+
+				$scope.newTodo = '';
+			};
+
+		//Supprimer un todo
+		$scope.removeTodo = function(todo){
+			todos.splice(todos.indexOf(todo), 1);
+		};
+		//Cocher Décocher
+		$scope.markAll = function(completed){
+			todos.forEach(function(todo){
+				todo.completed = !completed;
+			});
+		};
+
+		//Enlever les cases cochées
+		$scope.clearCompletedTodos = function(){
+			$scope.todos = todos = todos.filter(function(todo){
+				return !todo.completed;
+			});
+		};
+	}
+]);
